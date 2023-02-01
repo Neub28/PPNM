600542d6d811ba6348d40ea0adc611eda34420b0
Neub28 <nicolairneubert@gmail.com>
2023-02-01 11:45:20 +0100

    Math exercise has been updated

---
+++ Exercises/Math/main.cs
@@ -0,0 +1,14 @@
+using static System.Math;
+using static System.Console;
+class main{
+	static double sqrt2 = Sqrt(2.0);
+	static double exp = Exp(PI);
+	static double pi = Pow(PI, Exp(1));
+	
+	static void Main() {
+	Write($"sqrt(2) = {sqrt2}\n");
+	Write($"2 = {sqrt2*sqrt2}\n");
+	Write($"e^pi = {exp}\n");
+	Write($"pi^e = {pi}\n");
+	}
+}
