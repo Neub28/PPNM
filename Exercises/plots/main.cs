8e78ee223bf01f10985d52afb3d4a061cb74077f
Neub28 <124046805+Neub28@users.noreply.github.com>
2023-03-01 15:12:33 +0100

    Plots updated

---
+++ Exercises/plots/main.cs
@@ -0,0 +1,24 @@
+using static System.Console;
+
+class main{
+static void Main(string[] args) {
+	// Seperated the Main into taking an input handling the different 
+	// functions
+	
+	if(args[0] == "-erf") {
+	var outstream = new System.IO.StreamWriter("error_func.data");
+	for(double x = -5+1.0/128; x <= 5; x += 1.0/128) {
+		outstream.WriteLine($"{x} {sfuns.erf(x)}");
+	}
+	outstream.Close();
+	}
+
+	if(args[0] == "-gamma") {
+	var outstream = new System.IO.StreamWriter("gamma.data");
+	for(double x = -5+1.0/128; x <= 5; x += 1.0/128) {
+		outstream.WriteLine($"{x} {sfuns.gamma(x)}");
+	}
+	outstream.Close();
+	}
+	}
+}
