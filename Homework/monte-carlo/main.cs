f399a6ffd2876bbb32d84c04672b7908914351fe
Neub28 <124046805+Neub28@users.noreply.github.com>
2023-04-20 19:43:33 +0200

    Monte Carlo part b

--- Homework/monte-carlo/main.cs
+++ Homework/monte-carlo/main.cs
@@ -4,15 +4,18 @@ using static System.Console;
 using static System.Math;
 
 class main {
-
+	public static Func<vector, double> f;
+	
 	static void Main() {
 	partA();
+	partB();
 
 	}
 	static void partA() {
+	WriteLine("---------------------- Part A --------------------");
 	/* I need to test my plain monte carlo integrator */
 
-	Func<vector, double> f = delegate(vector x) {
+	f = delegate(vector x) {
 		if(x.norm() <= 1) return 1;
 		else return 0;
 	};
@@ -30,6 +33,54 @@ class main {
 
 	}
 	out1.Close();
+	WriteLine("A plot of the estimated error and actual error as functions of samplings points are in plainmc.svg.");
+
+	/* Trying to calculate the integral suggested  */
+	f = delegate(vector v) {
+		return Pow(PI, -3)*Pow(1-Cos(v[0])*Cos(v[1])*Cos(v[2]), -1);
+	};
+	(double intg, double er) = mcint.plainmc(f, 
+						new vector(0.0, 0.0, 0.0), 
+						new vector(PI, PI, PI), 100000);
+	WriteLine("Integral from 0 to pi of function 1/(pi³)[1-cos(x)cos(y)cos(z)]⁻1 dxdydz");
+	WriteLine($"Integral is equal to:	{intg} ± {er}.");
+	WriteLine($"The expcted result is:	1.3932039296856768591842462603255");
+	WriteLine($"The actual error is:	{intg-1.3932039296856768591842462603255}");
+
+	}
+
+	static void partB() {
+	WriteLine("---------------------- Part B --------------------");
+	
+	WriteLine("I want to test my implemented Quasi-random Monte-Carlo integrator.");
+	WriteLine("I want to integrate f(r,θ,φ)=16*r*cos(θ) over the upper half of a sphere with radius 1.");
+	WriteLine("The limits are r: [0:1], φ: [0: π/2], θ: [0: 2π]");
+	f = delegate(vector v) { return 16*Pow(v[0],3)*Cos(v[1])*Sin(v[1]);  };
+	vector aB = new vector(0.0, 0.0, 0.0);
+	vector bB = new vector(1.0, PI/2.0, 2.0*PI);
+	var outQ = new StreamWriter("qintg.txt");
+	var outP = new StreamWriter("pintg.txt");
+
+	int upperlim = 11000;
+	int step = 1000;
+	for(int N = 1000; N < upperlim; N += 1000) {
+		(double qintg, double qerr) = mcint.qrand(f, aB, bB, N);
+		(double pintg, double perr) = mcint.plainmc(f, aB, bB, N);
+
+		outQ.WriteLine($"{N}	{qintg}	{qerr}");
+		outP.WriteLine($"{N}	{pintg}	{perr}");
+
+		if(N == (upperlim - step)) {
+			WriteLine($"Results for N = {upperlim- step}");
+			WriteLine($"Analytical result:		{4*PI}");
+			WriteLine($"Quasi-random estimate:	{qintg} ± {qerr}");
+			WriteLine($"Plain Monte-Carlo estimate:	{pintg} ± {perr}");
+			WriteLine($"The abs. difference is:	{Abs(qintg-pintg)}");
+			}
+
+		}
+	outQ.Close();
+	outP.Close();
 
 	}
 
--- Homework/monte-carlo/mcint.cs
+++ Homework/monte-carlo/mcint.cs
@@ -2,14 +2,38 @@ using System;
 using static System.Math;
 
 public static class mcint {
+	// Variable of primes for Halton
+	public static int[] primes1 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61};
+	public static int[] primes2 = {67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131};
+	
+	/* Implementation of Van der Corput algorithm */
+	public static double corput(int n, int b) {
+	double q = 0;
+	double bk = (double) 1.0/b;
+	while(n > 0) { 
+		q += ( n % b ) * bk; 
+		n /= b;
+		bk /= b;
+	}
+	return q;
+	}
+	/* Halton-algorithm: Takes int-array of primes as sequence */
+	public static void halton(int n, int d, vector v, int[] prime, vector a, vector b) {
+	/* Fill values in vector v with Corput */
+	for(int i = 0; i < d; i++) {
+		v[i] = corput(n, prime[i])*(b[i]-a[i]);
+	}
+	}
 	
 	/* Plain Monte Carlo integrator ranging from vector a to vector b */
-	public static (double, double) plainmc
-	(Func<vector, double> f, vector a, vector b, int N) 
+	public static (double, double) plainmc(	Func<vector, double> f, 
+						vector a,
+						vector b, 
+						int N) 
 	{
 	int dim = a.size; 
 	double V = 1; 
-	for(int i = 0; i < dim; i++) V *= b[i]-a[i];
+	for(int i = 0; i < dim; i++) V *= Abs(b[i]-a[i]);
 	double sum = 0; 
 	double sum2 = 0;
 	var x = new vector(dim);
@@ -18,7 +42,9 @@ public static class mcint {
 		for(int k = 0; k < dim; k++) { 
 			x[k] = a[k] + rnd.NextDouble()*(b[k]-a[k]);
 		}
-		double fx = f(x); sum += fx; sum2 += fx*fx;
+		double fx = f(x); 
+		sum += fx; 
+		sum2 += fx*fx;
 	}
 	double mean = sum/N; 
 	double sigma = Sqrt(sum2/N-mean*mean);
@@ -26,6 +52,31 @@ public static class mcint {
 	var result = (mean*V, sigma*V/Sqrt(N));
 	
 	return result;
+
+	}
+	public static (double, double) qrand(	Func<vector, double> f,
+						vector a,
+						vector b,
+						int N )
+	{
+	int dim = a.size;
+	double V = 1;
+	for(int i = 0; i < dim; i++) V *= b[i]-a[i];
+	double sum = 0;
+	double sum2 = 0;
+	var v = new vector(dim);
+	for(int i = 0; i < N; i++) {
+		/* First sequence for integral */
+		halton(i, dim, v, primes1, a, b);
+		double fx = f(v);
+		sum += fx;
+		/* Second sequence for error estimation */
+		halton(i, dim, v, primes2, a, b);
+		sum2 += f(v);
+	}
+	double intg = sum/N*V;
+	double err = Abs(sum-sum2)/N*V;
+	return (intg, err);
 
 	}
 
